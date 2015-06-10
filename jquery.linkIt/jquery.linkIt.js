0a32544ee024d7c1da202f8251c1196e0b68ea7a
snehalKarmuse <snkarmuse@yahoo.com>
2015-06-10 20:24:30 +0100

    added index html2

--- jquery.linkIt/jquery.linkIt.js
+++ jquery.linkIt/jquery.linkIt.js
@@ -1,3 +1,28 @@
 /**
  * Created by Nitin on 10/06/2015.
  */
+(function($) {
+    $.fn.linkIt = function (option) {
+        var settings = $.extend({
+            href: null,
+            text: null,
+            target: '_self'
+        }, option);
+
+        if (settings.href == null) {
+            console.log("you need href option ");
+            return this;
+        }
+        return this.each(function () {
+            var object = $(this);
+            if (settings.text == null) {
+                settings.text = object.text();
+                object.wrap('<a target="' + settings.target + '"href="' + settings.href + '"></a>').text(settings.text);
+            }
+        })
+    }
+}(jQuery))
+
+
+
+
